da91dd8ace52984a64cd9016f629fa44cc0df080
alexpatapan <42231868+alexpatapan@users.noreply.github.com>
2019-11-30 18:13:26 +1000

    Add files via upload

--- asx company listing.py
+++ asx company listing.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.common.exceptions import TimeoutException
 
-browser = webdriver.Chrome(executable_path="/Users/alexpatapan/Documents/coding/Python/chromedriver")
+browser = webdriver.Chrome(executable_path="/Users/alexpatapan/coding/Python/chromedriver")
 
 while True:
     company = input('What company would you like to check the listing of? ')
@@ -16,17 +16,20 @@ while True:
     url = 'https://www.asx.com.au/asx/share-price-research/company/'+company
 
     browser.get(url)
-    
-    price_element = browser.find_elements_by_xpath("//*[@id=\"information-column\"]/div[4]/div[1]/div[1]/company-summary/table/tbody/tr[1]/td[1]/span")
-    name_element = browser.find_elements_by_xpath("//*[@id=\"company-name-title\"]")
 
-    print(name_element[0].text, 'has current share price of $' + price_element[0].text)
+    try: 
+        price_element = browser.find_elements_by_xpath("//*[@id=\"information-column\"]/div[4]/div[1]/div[1]/company-summary/table/tbody/tr[1]/td[1]/span")
+        name_element = browser.find_elements_by_xpath("//*[@id=\"company-name-title\"]")
 
-    url = 'https://www.google.com/search?q=' + name_element[0].text.replace(" ","+")
-    browser.get(url)
+        print(name_element[0].text, 'has current share price of $' + price_element[0].text)
 
-    try:
-        info = browser.find_elements_by_xpath("//*[@id=\"rhs\"]/div/div[1]/div[1]/div[1]/div/div[3]/div/div[1]/div/div/div/div/span")
+        url = 'https://www.google.com/search?q=' + name_element[0].text.replace(" ","+")
+        browser.get(url)
+
+    #try:
+        info = browser.find_elements_by_xpath("/html/body/div[7]/div[3]/div[10]/div[1]/div[3]/div/div[1]/div[1]/span/div/div/div[3]/div/div[1]/div/div/div/div/span[1]")
+
         print(info[0].text)
     except: 
-        pass
+     #   pass
+        print("Sorry, there was an issue retrieving this info")
